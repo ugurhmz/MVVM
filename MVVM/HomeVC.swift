eecb5d8e3541fb5ca4e53013a1b42860b84e3b67
ugurhmz <uhamzaoglu@fdnsoft.com>
2023-01-12 15:42:15 +0300

    init

--- MVVM/HomeVC.swift
+++ MVVM/HomeVC.swift
@@ -9,21 +9,15 @@ import UIKit
 
 class HomeVC: UIViewController {
 
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        welcomeLabel.text = "Welcome"
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
--- MVVM/LoginVC.swift
+++ MVVM/LoginVC.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class LoginVC: UIViewController {
-
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var infoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-
+    
+    
+    @IBAction func loginBtnAct(_ sender: UIButton) {
+        guard let name = nameTextField.text,
+              let password = passwordTextField.text else {
+                infoLabel.textColor = .red
+                infoLabel.text = "Name and password empty!"
+            return
+        }
+        
+        // viewModel -> login
+    }
+    
 }
 
